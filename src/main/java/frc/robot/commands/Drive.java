aa6e3f3d12203f603ccb3817f0abe95a5b2fb9a2
Sonakshi Jindal <sjindal23@claytonschools.stl>
2019-01-21 14:45:30 -0600

    made the motors move

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -10,23 +10,23 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
-/**
- * An example command.  You can replace me with your own command.
- */
-public class ExampleCommand extends Command {
-  public ExampleCommand() {
+public class Drive extends Command {
+  public Drive() {
     // Use requires() here to declare subsystem dependencies
-    requires(Robot.m_subsystem);
+    requires(Robot.drive);
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.drive.drive(Robot.m_oi.getJoyX(), Robot.m_oi.getJoyZ());
+  
   }
 
   // Make this return true when this Command no longer needs to run execute()
